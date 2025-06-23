174bef2facbbcee77bdce94ea5ba6610afea7d88
Emby <210781416+birch8@users.noreply.github.com>
2025-06-23 13:12:13 +1000

    Add CavalierKingCharlesSpaniel

--- doggo.py
+++ doggo.py
@@ -34,7 +34,23 @@ class Labrador(Dog):
         
     def walking_speed(self):
         return "Run"
-       
+
+# Teacher's personal favourite
+class CavalierKingCharlesSpaniel(Dog):
+    def __init__(self, name, age, size):
+        super().__init__(name, age, size) # Inherit __init__ from base class
+
+    def temperament(self):
+        if self.age < 2:
+            return "Flighty"
+        elif self.age <10:
+            return "Fun Loving"
+        else:
+            return "Getting slower"
+        
+    def walking_speed(self):
+        return "Bolt" # This dog is super fast and it BOLTS
+
 # Teacher's note: Don't use multiple inheritance if you can avoid it!
 # Results may be unpredictable and we don't like unpredictable things in software engineering!
 class LabraDoodle(Poodle,Labrador):
@@ -59,3 +75,7 @@ print(bob.walking_speed())
 basic_doggo = Dog('Henry', 4, 'Large')
 print(basic_doggo.name, basic_doggo.age, basic_doggo.size)
 print(basic_doggo.walking_speed())
+
+zeno = CavalierKingCharlesSpaniel('Zeno', 2, 'medium')
+print(zeno.temperament())
+print(zeno.walking_speed())
