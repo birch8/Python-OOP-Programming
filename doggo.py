7e93a02c2b7e6b47ec4e5eb7f27add12469e07c8
birch8 <210781416+birch8@users.noreply.github.com>
2025-06-27 12:16:32 +1000

    Make allergen_check method in Dog class

--- doggo.py
+++ doggo.py
@@ -3,6 +3,7 @@
 class Dog:
     likes_walks = True
     walking_speed = "Walk"
+    is_low_allergen = False # By default the dog is NOT low allergen
     def __init__(self, name, age, size):
         self.name = name
         self.age = age
@@ -18,12 +19,18 @@ class Dog:
     def introduce(self):
         print(f"#### {self.name}'s information ####")
         return f"WOOF! My name is {self.name}. I'm {self.age} years old and my size is {self.size}!"
+    
+    def allergen_check(self): # Allergen check
+        if self.is_low_allergen == True:
+            return "I'm low allergen!"
+        else:
+            return "I'm NOT low allergen! Am I high allergen??"
 
 # Subclasses of Dog are defined below
 # Make a poodle subclass, which is low allergen and prances
 class Poodle(Dog): 
     walking_speed = "Prance"
-    is_low_allergen = True # Use attribute instead of method
+    is_low_allergen = True # Change low allergen to True for poodles
 
     def __init__(self, name, age, size):
         super().__init__(name, age, size)
@@ -61,6 +68,8 @@ class LabraDoodle(Poodle,Labrador):
     # LabraDoodles by default inherit the walking_speed of the Poodle (it's the first argument) - in this case it's a Prance
     def __init__(self, name, age, size):
         super().__init__(name, age, size)
+    
+    # Currently the LabraDoodle also has inherited the temperament method from Labrador, which isn't found in Poodle.
 
 ################################################
 sam = Dog('Sam', 5, 'enormous')
@@ -68,17 +77,17 @@ print(sam.introduce())
 
 jerry = Dog('Jerry', 3, 'wide')
 print(jerry.introduce())
+print(jerry.allergen_check())
 
 bob = Poodle('Bob', 2, 'small')
 print(bob.introduce())
 print(f"Bob likes walks: {bob.likes_walks}")
-print(f"Bob is low allergen: {bob.is_low_allergen}")
+print(bob.allergen_check())
 print(bob.go_on_walk())
 
-print("Sussy doggo")
 swiftie = LabraDoodle('Swiftie', 1, "Medium")
 print(swiftie.introduce())
-print(swiftie.is_low_allergen)
+print(swiftie.allergen_check())
 print(swiftie.temperament())
 print(swiftie.go_on_walk())
 
