ecf85295170e48a071d7b17ca6b06a4cf4ab75bf
birch8 <210781416+birch8@users.noreply.github.com>
2025-06-27 12:03:39 +1000

    Change walking_speed method to attribute
    Now when you want to go on a walk with the dog you can run the new go_on_walk() method where you first ask the dog to go on a walk and it will walk at its predefined walking_speed

--- doggo.py
+++ doggo.py
@@ -2,28 +2,33 @@
 # Methods of likes_walks and walking_speed
 class Dog:
     likes_walks = True
+    walking_speed = "Walk"
     def __init__(self, name, age, size):
         self.name = name
         self.age = age
         self.size = size
     
-    def walking_speed(self):
-        return "Walk"
+    def go_on_walk(self):
+        print(f"Kindly requesting {self.name} to go on a walk...")
+        if self.likes_walks == True:
+            return f"WOOF! I love walks! Let's walk at this speed: {self.walking_speed}"
+        else:
+            return f"WOOF! Not a fan of walks myself."
     
     def introduce(self):
         return f"WOOF! My name is {self.name}. I'm {self.age} years old and my size is {self.size}!"
 
 # Subclasses of Dog are defined below
-# Make a poodle subclass, which is "low alergen" and prances
+# Make a poodle subclass, which is low allergen and prances
 class Poodle(Dog): 
+    walking_speed = "Prance"
     is_low_allergen = True # Use attribute instead of method
+
     def __init__(self, name, age, size):
         super().__init__(name, age, size)
-
-    def walking_speed(self): # Inherit the method but override the return
-        return "Prance"
             
 class Labrador(Dog):
+    walking_speed = "Run"
     def __init__(self, name, age, size):
         super().__init__(name, age, size)
 
@@ -34,12 +39,10 @@ class Labrador(Dog):
             return "Fun Loving"
         else:
             return "Getting slower"
-        
-    def walking_speed(self):
-        return "Run"
 
 # Teacher's personal favourite
 class CavalierKingCharlesSpaniel(Dog):
+    walking_speed = "Bolt" # This dog is really fast and it BOLTS
     def __init__(self, name, age, size):
         super().__init__(name, age, size) # Inherit __init__ from base class
 
@@ -50,9 +53,6 @@ class CavalierKingCharlesSpaniel(Dog):
             return "Fun Loving"
         else:
             return "Getting slower"
-        
-    def walking_speed(self):
-        return "Bolt" # This dog is super fast and it BOLTS
 
 # Teacher's note: Don't use multiple inheritance if you can avoid it!
 # Results may be unpredictable and we don't like unpredictable things in software engineering!
@@ -75,7 +75,7 @@ print(jerry.introduce())
 bob = Poodle('Bob', 2, 'small')
 print(bob.name, bob.age, bob.size)
 print(bob.likes_walks)
-print(bob.walking_speed())
+print(bob.go_on_walk())
 print(bob.is_low_allergen)
 
 print("Sussy doggo")
@@ -83,15 +83,15 @@ swiftie = LabraDoodle('Swiftie', 1, "Medium")
 print(swiftie.name, swiftie.age, swiftie.size)
 print(swiftie.is_low_allergen)
 print(swiftie.temperament())
-print(swiftie.walking_speed())
+print(swiftie.go_on_walk())
 
 print("Basic doggo")
 basic_doggo = Dog('Henry', 4, 'Large')
 print(basic_doggo.name, basic_doggo.age, basic_doggo.size)
-print(basic_doggo.walking_speed())
+print(basic_doggo.go_on_walk())
 
 # Instantiate a CavalierKingCharlesSpaniel object called Zeno
 print("Zeno's information:")
 zeno = CavalierKingCharlesSpaniel('Zeno', 2, 'medium')
 print(zeno.temperament())
-print(zeno.walking_speed())
+print(zeno.go_on_walk())
