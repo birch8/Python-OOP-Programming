fcc62838062875c9d8a85510599d4740806a29f5
birch8 <210781416+birch8@users.noreply.github.com>
2025-06-30 19:46:03 +1000

    Add comments
    across the entire program. also removed basic doggo (commented out)

--- doggo.py
+++ doggo.py
@@ -1,32 +1,32 @@
 # Define a class called dog. Basic attributes of name, age, size.
 # Methods of likes_walks and walking_speed
 class Dog:
-    likes_walks = True
-    walking_speed = "Walk"
+    likes_walks = True # By default, dogs like walks.
+    walking_speed = "Walk" # By default, the dog walks at the walking_speed of "Walk"
     is_low_allergen = False # By default the dog is NOT low allergen
     def __init__(self, name, age, size):
         self.name = name
         self.age = age
         self.size = size
     
-    def go_on_walk(self):
+    def go_on_walk(self): # Calling this method causes the dog to go on a walk (if it likes walks), and returns its walking speed.
         print(f"Kindly requesting {self.name} to go on a walk...")
         if self.likes_walks == True:
             return f"WOOF! I love walks! Let's walk at this speed: {self.walking_speed}"
         else:
             return f"WOOF! Not a fan of walks myself."
     
-    def introduce(self):
+    def introduce(self): # Calling this method causes the dog to introduce itself by saying its name, age and size.
         print(f"#### {self.name}'s information ####")
         return f"WOOF! My name is {self.name}. I'm {self.age} years old and my size is {self.size}!"
     
-    def allergen_check(self): # Allergen check
+    def allergen_check(self): # Calling this method causes the dog to say whether it is low allergen or not.
         if self.is_low_allergen == True:
             return "WOOF! I'm low allergen!"
         else:
             return "WOOF! I'm NOT low allergen! Am I high allergen??"
         
-    def temperament(self):
+    def temperament(self): # Calling this method returns the dog's temperament.
         print(f"{self.name}'s temperament is:")
         if self.age < 2:
             return "Jumpy"
@@ -38,20 +38,20 @@ class Dog:
 # Subclasses of Dog are defined below
 # Make a poodle subclass, which is low allergen and prances
 class Poodle(Dog): 
-    walking_speed = "Prance"
+    walking_speed = "Prance" # Override the default walking_speed and replace it with Prance
     is_low_allergen = True # Change low allergen to True for poodles
 
     def __init__(self, name, age, size):
-        super().__init__(name, age, size)
+        super().__init__(name, age, size) # Inherit __init__ from base class
             
 class Labrador(Dog):
-    walking_speed = "Run"
+    walking_speed = "Run" # Override the default walking_speed and replace it with Run
     def __init__(self, name, age, size):
-        super().__init__(name, age, size)
+        super().__init__(name, age, size) # Inherit __init__ from base class
 
     def temperament(self):
         print(f"{self.name}'s temperament is:")
-        if self.age < 2:
+        if self.age < 2: # Labrador inherits the temperament method but overrides the values returned.
             return "Flighty"
         elif self.age <10:
             return "Fun Loving"
@@ -64,7 +64,7 @@ class CavalierKingCharlesSpaniel(Dog):
     def __init__(self, name, age, size):
         super().__init__(name, age, size) # Inherit __init__ from base class
 
-    def temperament(self):
+    def temperament(self): # CavalierKingCharlesSpaniel inherits the temperament method but overrides the values returned.
         print(f"{self.name}'s temperament is:")
         if self.age < 2:
             return "Energetic"
@@ -82,32 +82,32 @@ class LabraDoodle(Poodle,Labrador):
     
     # Currently the LabraDoodle also has inherited the Flighty part of the temperament method from Labrador, which isn't found in Poodle.
 
-################################################
-sam = Dog('Sam', 5, 'enormous')
-print(sam.introduce())
+#################### Creating objects and calling methods ##############################
+sam = Dog('Sam', 5, 'enormous') # First example doggo
+print(sam.introduce()) # Make Sam introduce itself
 
-jerry = Dog('Jerry', 3, 'wide')
+jerry = Dog('Jerry', 3, 'wide') # Second example doggo
 print(jerry.introduce())
-print(jerry.allergen_check())
+print(jerry.allergen_check()) # Check if Jerry is low allergen
 
-bob = Poodle('Bob', 2, 'small')
+bob = Poodle('Bob', 2, 'small') # Third example doggo (poodle)
 print(bob.introduce())
-print(f"Bob likes walks: {bob.likes_walks}")
-print(bob.allergen_check())
-print(bob.go_on_walk())
+print(f"Bob likes walks: {bob.likes_walks}") # Check if Bob likes walks
+print(bob.allergen_check()) 
+print(bob.go_on_walk()) # Let Bob go on a walk
 
-swiftie = LabraDoodle('Swiftie', 1, "Medium")
+swiftie = LabraDoodle('Swiftie', 1, "Medium") # Fourth example doggo (LabraDoodle)
 print(swiftie.introduce())
 print(swiftie.allergen_check())
-print(swiftie.temperament())
+print(swiftie.temperament()) # Check Swiftie's temperament
 print(swiftie.go_on_walk())
 
-basic_doggo = Dog('Henry', 4, 'Large')
-print(basic_doggo.introduce())
-print(basic_doggo.go_on_walk())
+# basic_doggo = Dog('Henry', 4, 'Large')
+# print(basic_doggo.introduce())
+# print(basic_doggo.go_on_walk())
 
 # Instantiate a CavalierKingCharlesSpaniel object called Zeno
-zeno = CavalierKingCharlesSpaniel('Zeno', 2, 'medium')
+zeno = CavalierKingCharlesSpaniel('Zeno', 2, 'medium') # Fifth example doggo (CavalierKingCharlesSpaniel)
 print(zeno.introduce())
 print(zeno.temperament())
 print(zeno.go_on_walk())
